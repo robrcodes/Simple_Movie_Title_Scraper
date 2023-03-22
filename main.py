5eeaf96ce9dfabf35373db6cb8c556a4e4fbd6ab
Rob <robjrenwick@gmail.com>
2023-03-22 19:54:31 +1000

    scrape titles from each page
    identify and scrape movie titles from all pages

--- main.py
+++ main.py
@@ -19,3 +19,25 @@ pages = pagination.find_all('a', class_='rc5')
 # pages[-2]  # last useful element that we need
 last_page = pages[-2].text
 print(f'Total pages to scrape: {last_page}')
+
+# example of page url
+# page 2 url = https://www.scripts.com/scripts/A/2
+
+movie_list = []  # list to hold movie titles once scraped
+
+for page in range(1, int(last_page)+1):  # 1 to last page
+    titles_website = f'{website}/{page}'
+    result = requests.get(titles_website)
+    content = result.text
+    soup = bs(content, 'lxml')
+
+    # box for content of on page <table> element
+    box_table = soup.find('table', class_='tdata')
+
+    # all title text inside <strong> tags
+    titles = box_table.find_all('strong')
+
+    # feedback while running to display progress
+    print(f'Scraping page number: {page}')
+
+    
